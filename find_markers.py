ea0778346dd59c767a74491f6dc64599189f5292
Grintsov <@email.com>
2026-02-18 15:22:47 +0300

    Cleanup: удаляем мусор

--- find_markers.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-df = pd.read_excel('c:/Users/Grintsov/Pyton/Griin/Data.xlsx', header=None)
-# Найдем строки с маркерами
-for i, row in df.iterrows():
-    row_str = ' '.join(str(c) for c in row if pd.notna(c))
-    if 'К4' in row_str or 'К2' in row_str or 'К3' in row_str or 'Л3' in row_str:
-        print(f'{i}: {row.tolist()}')
